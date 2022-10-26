5a790b40a18d908746becff84d470b5d375e812a
Irina Stelea <capatul.lumii@gmail.com>
2022-10-26 18:43:22 +0200

    responded to reviews. #3

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 🍣 🍣 🍣" />
+      <HelloWorld msg="You did it! 🍣 🍣 🍣 Veggie sushi sucks" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>

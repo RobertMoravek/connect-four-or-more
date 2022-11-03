015441471468f16f1ebcf8642f5e1fbcd3b91a26
Irina Stelea <capatul.lumii@gmail.com>
2022-11-03 12:45:52 +0100

    tested horiz, vert & diag sets with real examples. #23

--- src/checkVictory.ts
+++ src/checkVictory.ts
@@ -11,28 +11,20 @@ export function checkForVictory(
     let colCount: number = config[0];
     let rowCount: number = config[1];
     let nbWin: number = config[2];
+
     //vertical set
     let verticalSetArray: number[][] = gameBoard[colIdx]
         .slice(0, rowIdx + 1)
         .map((e, idx) => [colIdx, idx, e]);
 
     //horizontal set
-    // let horizontalSet: player[] = [].concat(
-    //     ...gameBoard.map((e) => e.slice(rowIdx, rowIdx + 1))
-    // );
-    // let horizontalSetArray: number[][] = horizontalSet.map((e, idx) => [
-    //     idx,
-    //     rowIdx,
-    //     e,
-    // ]);
-
     let horizontalSetArray: number[][] = gameBoard.map((e, idx) =>
         [idx, rowIdx, e.slice(rowIdx, rowIdx + 1)].flat()
     );
 
     //diagonal sets
     //1. backward diagonal - find the starting slot
-    let diagBackSetArray: number[][];
+    let diagBackSetArray: number[][] = [];
     while (colIdx > 0 && rowIdx < rowCount - 1) {
         colIdx -= 1;
         rowIdx += 1;
@@ -46,11 +38,9 @@ export function checkForVictory(
         rowIdx -= 1;
         diagBackSetArray.push([colIdx, rowIdx, gameBoard[colIdx][rowIdx]]);
     }
-    // let diagBack: player[];
-    // diagBack = [].concat(...diagBackSetArray.map((e) => e[2]));
 
     //2. forward diagonal - find the starting slot
-    let diagFwdSetArray: number[][];
+    let diagFwdSetArray: number[][] = [];
     while (colIdx > 0 && rowIdx > 0) {
         colIdx -= 1;
         rowIdx -= 1;
@@ -64,19 +54,17 @@ export function checkForVictory(
         rowIdx += 1;
         diagFwdSetArray.push([colIdx, rowIdx, gameBoard[colIdx][rowIdx]]);
     }
-    // let diagFwd: player[];
-    // diagFwd = [].concat(...diagFwdSetArray.map((e) => e[2]));
 
-    //actual function where paramSet is the set that gets passed
+    //getting the slot values out of the whole set
     // let set = [].concat(...paramSet.map((e) => e[2]));
 
-    //avoid unnecessary checks for shorter sets
+    //???avoid unnecessary checks for shorter sets
     // if (set.length < nbWin) {
     //     return false;
     // }
 
     let count = 0;
-    let winningSlots: [number, number][] | null;
+    let winningSlots: [number, number][] | null = [];
 
     for (let slot of paramSet) {
         if (slot[2] == playerTurn) {
